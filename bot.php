ec90552156529e0d5468a3bb07b3a26f8568259a
wckjgithub <k_watcharin@hotmail.co.th>
2017-11-26 08:45:59 +0700

    Change
    Change

--- bot.php
+++ bot.php
@@ -19,7 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => '©Ñ¹¨Ð¨ÓäÇé'
+				//'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
