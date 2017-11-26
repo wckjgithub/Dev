850f32eb8232227c1b77c48defc3f41983e1b84c
wckjgithub <k_watcharin@hotmail.co.th>
2017-11-26 09:11:12 +0700

    a
    a

--- bot.php
+++ bot.php
@@ -13,13 +13,14 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$userId = $event['message']['userId']
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'pop test'
+				'text' => $userId.'pop test'
 				//'text' => $text
 			];
 
